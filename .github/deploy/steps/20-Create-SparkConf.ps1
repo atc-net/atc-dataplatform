b8475dcbf3951241b10c2fed1adb44e04e7d3fa0
Lau Johansson <Lau_johansson@hotmail.com>
2022-11-01 08:14:19 +0100

    dynamic sparkconffile

---
+++ .github/deploy/steps/20-Create-SparkConf.ps1
@@ -0,0 +1,16 @@
+Write-Host "Write cluster configuration for Direct Access..." -ForegroundColor DarkYellow
+$confDirectAccess = [ordered]@{}
+
+$confDirectAccess["spark.databricks.cluster.profile"]= "singleNode"
+$confDirectAccess["spark.databricks.delta.preview.enabled"] = $true
+$confDirectAccess["spark.databricks.io.cache.enabled"] = $true
+$confDirectAccess["spark.master"]= "local[*, 4]"
+
+Write-Host "  Adds Direct Access for $resourceName..." -ForegroundColor DarkYellow
+$confDirectAccess["fs.azure.account.oauth2.client.id.$resourceName.dfs.core.windows.net"] = "{{secrets/secrets/DatabricksClientId}}"
+$confDirectAccess["fs.azure.account.oauth2.client.endpoint.$resourceName.dfs.core.windows.net"] = "{{secrets/secrets/DatabricksOauthEndpoint}}"
+$confDirectAccess["fs.azure.account.oauth.provider.type.$resourceName.dfs.core.windows.net"] = "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider"
+$confDirectAccess["fs.azure.account.oauth2.client.secret.$resourceName.dfs.core.windows.net"] = "{{secrets/secrets/DatabricksClientSecret}}"
+$confDirectAccess["fs.azure.account.auth.type.$resourceName.dfs.core.windows.net"] = "OAuth"
+
+Set-Content .\.github\submit\sparkconf.json ($confDirectAccess | ConvertTo-Json)
