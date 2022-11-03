d3b58c844ae5e7b74d8d771a89edd406b7174278
Simon Heisterkamp <set>
2022-11-03 14:55:26 +0000

    get storage uri from az cli

--- .github/deploy/steps/91-Create-SparkConf.ps1
+++ .github/deploy/steps/91-Create-SparkConf.ps1
@@ -9,8 +9,13 @@ $confDirectAccess["spark.databricks.delta.preview.enabled"] = $true
 $confDirectAccess["spark.databricks.io.cache.enabled"] = $true
 $confDirectAccess["spark.master"]= "local[*, 4]"
 
-Write-Host "  Adds Direct Access for $resourceName..." -ForegroundColor DarkYellow
-$storageUrl = "$resourceName.dfs.core.windows.net"
+
+$url = az storage account show --name $dataLakeName --resource-group $resourceGroupName  --query "primaryEndpoints.dfs" --out tsv
+
+$storageUrl = ([System.Uri]$url).Host
+
+Write-Host "  Adds Direct Access for $storageUrl..." -ForegroundColor DarkYellow
+
 $confDirectAccess["fs.azure.account.auth.type.$storageUrl"] = "OAuth"
 $confDirectAccess["fs.azure.account.oauth.provider.type.$storageUrl"] = "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider"
 $confDirectAccess["fs.azure.account.oauth2.client.id.$storageUrl"] = "{{secrets/secrets/DatabricksClientId}}"
