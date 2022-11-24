a4e0744a3fdf0ac8e0c9d7e763b076fd3e3a7c90
Lau Johansson <Lau_johansson@hotmail.com>
2022-11-24 14:52:18 +0100

    fix cache test

--- tests/cluster/cache/test_cache.py
+++ tests/cluster/cache/test_cache.py
@@ -26,7 +26,7 @@ class ChildCacher(CachedLoader):
         target_name = Configurator().table_name("CachedTestTarget")
         self.to_be_deleted = df
         Spark.get().sql(f"DELETE FROM {target_name} WHERE b = 8")
-        self.deleted = df.filter(df["b"] == 8)
+        self.deleted = df.filter(df["cache.b"] == 8)
         return self.deleted
 
 
