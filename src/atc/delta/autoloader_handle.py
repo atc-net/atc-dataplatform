850864f56a9bc03d06192152a710343cfc6ff5b8
Lau Johansson <Lau_johansson@hotmail.com>
2022-11-24 12:56:57 +0100

    remove self read in write method

--- src/atc/delta/autoloader_handle.py
+++ src/atc/delta/autoloader_handle.py
@@ -60,7 +60,9 @@ class AutoLoaderHandle(SparkHandle):
 
         return reader
 
-    def write_or_append(self, mode: str, mergeSchema: bool = None) -> None:
+    def write_or_append(
+        self, df: DataFrame, mode: str, mergeSchema: bool = None
+    ) -> None:
         #######################################
         # Note that there is no input dataframe
         #######################################
@@ -69,14 +71,10 @@ class AutoLoaderHandle(SparkHandle):
         if mode == "overwrite":
             mode = "complete"
 
-        # Before writing
-        # One needs to read the stream before
-        writer = self.read()
-
         # What if someone wants a select filter?
 
         writer = (
-            writer.writeStream.option("checkpointLocation", self._checkpoint_path)
+            df.writeStream.option("checkpointLocation", self._checkpoint_path)
             .outputMode(mode)
             .trigger(availableNow=True)
         )
@@ -86,10 +84,10 @@ class AutoLoaderHandle(SparkHandle):
         writer.awaitTermination()
 
     def overwrite(self, df: DataFrame, mergeSchema: bool = None) -> None:
-        return self.write_or_append("complete", mergeSchema)
+        return self.write_or_append(df, "complete", mergeSchema)
 
     def append(self, df: DataFrame, mergeSchema: bool = None) -> None:
-        return self.write_or_append("append", mergeSchema)
+        return self.write_or_append(df, "append", mergeSchema)
 
     # Truncate checkpoints too
     def truncate(self) -> None:
