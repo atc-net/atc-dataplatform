bcbed26b80958e54c9c5349f44dc23241a79b3d4
Lau Johansson <Lau_johansson@hotmail.com>
2022-11-28 09:24:51 +0100

    feat: add avrofiles using write avro

--- tests/cluster/delta/test_autoloader_handle.py
+++ tests/cluster/delta/test_autoloader_handle.py
@@ -1,4 +1,6 @@
 import unittest
+import uuid as _uuid
+from typing import List, Tuple
 
 from pyspark.sql.utils import AnalysisException
 
@@ -16,21 +18,34 @@ from tests.cluster.values import resourceName
 
 
 class AutoloaderTests(unittest.TestCase):
-    eh_checkpoint_path = "/mnt/atc/silver/eh/_checkpoint_path"
+    avrosource_checkpoint_path = (
+        f"/mnt/{resourceName()}/silver/{resourceName()}"
+        f"/avrolocation/_checkpoint_path_avro"
+    )
+
+    avro_source_path = (
+        f"/mnt/{resourceName()}/silver/{resourceName()}/avrolocation/AvroSource"
+    )
 
     @classmethod
     def setUpClass(cls) -> None:
         Configurator().clear_all_configurations()
         Configurator().set_debug()
 
-        if not file_exists(cls.eh_checkpoint_path):
-            init_dbutils().fs.mkdirs(cls.eh_checkpoint_path)
+        if not file_exists(cls.avrosource_checkpoint_path):
+            init_dbutils().fs.mkdirs(cls.avrosource_checkpoint_path)
+
+        if not file_exists(cls.avro_source_path):
+            init_dbutils().fs.mkdirs(cls.avro_source_path)
 
     @classmethod
     def tearDownClass(cls) -> None:
         DbHandle.from_tc("MyDb").drop_cascade()
-        if file_exists(cls.eh_checkpoint_path):
-            init_dbutils().fs.rm(cls.eh_checkpoint_path, True)
+        if file_exists(cls.avrosource_checkpoint_path):
+            init_dbutils().fs.rm(cls.avrosource_checkpoint_path, True)
+
+        if file_exists(cls.avro_source_path):
+            init_dbutils().fs.rm(cls.avro_source_path, True)
         stop_all_streams()
 
     def test_01_configure(self):
@@ -92,30 +107,28 @@ class AutoloaderTests(unittest.TestCase):
             },
         )
 
-        eh_checkpoint_path = "/mnt/atc/silver/eh/_checkpoint_path"
-        init_dbutils().fs.mkdirs(eh_checkpoint_path)
-
         # add eventhub
         tc.register(
-            "AtcEh",
+            "AvroSource",
             {
-                "name": "AtcEh",
-                "path": f"/mnt/{resourceName()}/silver/{resourceName()}/atceh",
+                "name": "AvroSource",
+                "path": self.avro_source_path,
                 "format": "avro",
                 "partitioning": "ymd",
-                "checkpoint_path": self.eh_checkpoint_path,
+                "checkpoint_path": self.avrosource_checkpoint_path,
             },
         )
 
-        init_dbutils().fs.mkdirs(eh_checkpoint_path)
+        sink_checkpoint_path = "/mnt/atc/silver/testdb{ID}/_checkpoint_path_avrosink"
+        init_dbutils().fs.mkdirs(sink_checkpoint_path)
         # add eventhub sink
         tc.register(
-            "EhSink",
+            "AvroSink",
             {
-                "name": "TestDb{ID}.EhSink",
-                "path": "/mnt/atc/silver/testdb{ID}/EhSink",
+                "name": "TestDb{ID}.AvroSink",
+                "path": "/mnt/atc/silver/testdb{ID}/AvroSink",
                 "format": "delta",
-                "checkpoint_path": "/mnt/atc/silver/testdb{ID}/_checkpoint_path_ehsink",
+                "checkpoint_path": sink_checkpoint_path,
             },
         )
 
@@ -127,8 +140,8 @@ class AutoloaderTests(unittest.TestCase):
         AutoLoaderHandle.from_tc("MyTbl3")
         AutoLoaderHandle.from_tc("MyTbl4")
         AutoLoaderHandle.from_tc("MyTbl5")
-        AutoLoaderHandle.from_tc("AtcEh")
-        AutoLoaderHandle.from_tc("EhSink")
+        AutoLoaderHandle.from_tc("AvroSource")
+        AutoLoaderHandle.from_tc("AvroSink")
 
     def test_02_write_data_with_deltahandle(self):
         self._overwrite_two_rows_to_table("MyTbl")
@@ -193,7 +206,9 @@ class AutoloaderTests(unittest.TestCase):
 
     def test_09_read_avro(self):
 
-        ah_sink = AutoLoaderHandle.from_tc("EhSink")
+        self._add_avro_data_to_source([(1, "a"), (2, "b")])
+
+        ah_sink = AutoLoaderHandle.from_tc("AvroSink")
         Spark.get().sql(
             f"""
                     CREATE TABLE {ah_sink.get_tablename()}
@@ -206,14 +221,26 @@ class AutoloaderTests(unittest.TestCase):
 
         o = Orchestrator()
         o.extract_from(
-            SimpleExtractor(AutoLoaderHandle.from_tc("AtcEh"), dataset_key="AtcEh")
+            SimpleExtractor(
+                AutoLoaderHandle.from_tc("AvroSource"), dataset_key="AvroSource"
+            )
         )
         o.load_into(SimpleLoader(ah_sink, mode="append"))
         o.execute()
 
-        result = DeltaHandle.from_tc("EhSink").read()
+        result = DeltaHandle.from_tc("AvroSink").read()
 
-        self.assertTrue(0 < result.count())
+        self.assertTrue(2, result.count())
+
+        # Run again. Should not append more.
+        o.execute()
+        self.assertTrue(2, result.count())
+
+        self._add_avro_data_to_source([(3, "c"), (4, "d")])
+
+        # Run again. Should append.
+        o.execute()
+        self.assertTrue(4, result.count())
 
     def test_10_partitioning(self):
         dh = AutoLoaderHandle.from_tc("MyTbl4")
@@ -263,3 +290,8 @@ class AutoloaderTests(unittest.TestCase):
                             )
                         """
         )
+
+    def _add_avro_data_to_source(self, input_data: List[Tuple[int, str]]):
+        df = Spark.get().createDataFrame(input_data, "id int, name string")
+
+        df.write.format("avro").save(self.avro_source_path + "/" + str(_uuid.uuid4()))
