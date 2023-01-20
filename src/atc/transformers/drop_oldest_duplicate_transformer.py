dfe35816627d74e7198713e38f8cf7f718648eaa
Jeppe Blixenkrone-Møller <jeppe.blixen@gmail.com>
2023-01-20 11:38:14 +0100

    refactor: Update all transformers to changes in base Transformer

--- src/atc/transformers/drop_oldest_duplicate_transformer.py
+++ src/atc/transformers/drop_oldest_duplicate_transformer.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 
 from pyspark.sql import DataFrame
 
@@ -7,8 +7,38 @@ from atc.utils import DropOldestDuplicates
 
 
 class DropOldestDuplicatesTransformer(Transformer):
-    def __init__(self, *, cols: List[str], orderByColumn: str):
-        super().__init__()
+    """
+    This transformer drops the oldest duplicates based on some ordering.
+
+    Attributes:
+    ----------
+        cols : List[str]
+            List of columns to use for deduplication
+        orderByColumn : str
+            Column used for ordering
+        dataset_input_keys : Union[str, List[str]]
+            list of input dataset keys
+        dataset_output_key : str
+            output dataset key
+        consume_inputs : bool
+            Flag to control dataset consuming behavior
+    """
+
+    def __init__(
+        self,
+        *,
+        cols: List[str],
+        orderByColumn: str,
+        dataset_input_keys: Union[str, List[str]] = None,
+        dataset_output_key: str = None,
+        consume_inputs: bool = True,
+    ):
+        super().__init__(
+            dataset_input_keys=dataset_input_keys,
+            dataset_output_key=dataset_output_key,
+            consume_inputs=consume_inputs,
+        )
+
         self.cols = cols
         self.orderByColumn = orderByColumn
 
